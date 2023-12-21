deca220fbb9f3ea015e198442de0fb1f6c63d696
rupayanxi5567 <129682438+rupayanxi5567@users.noreply.github.com>
2023-12-21 08:34:04 +0000

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -6,6 +8,15 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/insta', (req, res) => {
+  res.send('Hello Insta mal!')
+})
+
+app.get('/fb', (req, res) => {
+  res.send('Hello chai aur coding !')
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Home and insta app listening on port ${port}`)
 })
